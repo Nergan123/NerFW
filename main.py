ab03d3de86aad3c115eb340f9ad8b84446057154
Nergan123 <moskalenkovladimir1998@gmail.com>
2023-05-17 20:21:51 +0200

    Transited to pure python instead of rendered json

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+from nerfw import Game
 from nerfw.game.ui.functions import Functions
 from nerfw.helpers import LoggerBase
 from nerfw.nerfw import NerFW
 
 
+def script(last_line):
+    """
+    Game script to be run
+    :return: None
+    """
+
+    app = Game(last_line)
+    app.ui.add_button("next", (10, 10), Functions.FORWARD)
+    app.ui.add_button("next2", (90, 70), Functions.FORWARD)
+    app.ui.compile()
+
+    tester = app.create_character("Tester", "test_files/test.jpg", (100, 255, 255))
+    coder = app.create_character("Coder", "test_files/test.jpg", (255, 100, 100))
+
+    tester.say("Test 1")
+    tester.say("Test 2")
+
+    coder.say("What are you testing?")
+
+
 if __name__ == "__main__":
     LoggerBase.setup_logger()
-    app = NerFW()
-    app.ui.add_button("next", (10, 10), Functions.FORWARD)
-    app.ui.add_button("next2", (10, 50), Functions.FORWARD)
-    app.ui.compile()
-    Tester = app.create_character("Tester", "test_files/test.jpg", (100, 255, 255))
-    Coder = app.create_character("Coder", "test_files/test.jpg", (255, 100, 100))
-
-    Tester.say("Test 1")
-    Tester.say("Test 2")
-
-    Coder.say("What are you testing?")
-
-    app.run()
+    ner = NerFW()
+    ner.run(script)
--- nerfw/__init__.py
+++ nerfw/__init__.py
@@ -1,1 +1,3 @@
 from .nerfw import NerFW
+from .game.game import Game
+from .game.ui.functions import Functions
