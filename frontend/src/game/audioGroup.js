d679cca7655f248d30a09cd9d221d3f670028f66
Nergan123 <moskalenkovladimir1998@gmail.com>
2023-12-12 23:26:34 +0100

    Audio switching

--- frontend/src/game/audioGroup.js
+++ frontend/src/game/audioGroup.js
@@ -3,21 +3,41 @@ import { useState, useEffect } from "react";
 
 function AudioGroup({sourcesInput}){
 
-    console.log(sourcesInput)
-
     const [sources, setSources] = useState([]);
     const initialState = new Array(sources.length).fill(false);
     const [playing, setPlaying] = useState(initialState)
-    const handldEnded = () => setPlaying(false);
+
+    function checkSourceInList(){
+        const links = sourcesInput.map((url) => {
+            var loc = window.location.href;
+            loc = loc.replace("game", url);
+            return loc
+        });
+        const newSources = [...sources];
+        sources.map((source, idxToDelete) => {
+            const idx = links.findIndex(e => e === source.source);
+            if(idx <= -1){
+                const filler = [...playing];
+                filler[idx] = false
+                source.player.removeEventListener('ended', () => setPlaying(filler));
+                source.player.pause();
+                newSources.splice(idxToDelete, 1);
+            }
+            return null
+        });
+        setSources(newSources);
+    };
 
     function handleSources(){
-
+        if(sourcesInput.length === 0 || sourcesInput.includes("")){
+            return null
+        }
         const processedSources = [...sources];
         sourcesInput.map((source) =>{
                 var loc = window.location.href;
                 loc = loc.replace("game", source);
                 const idx = sources.findIndex(e => e.source === loc);
-                if(idx <= -1){
+                if(idx <= -1 && !playing[idx]){
                     const audio = {
                         source: loc,
                         player: new Audio(),
@@ -26,7 +46,9 @@ function AudioGroup({sourcesInput}){
                     audio.player.type = "audio/mp3";
                     audio.player.load();
                     audio.player.play();
-                    audio.player.addEventListener('ended', handldEnded, false);
+                    const filler = [...playing];
+                    filler[idx] = true
+                    audio.player.addEventListener('ended', () => setPlaying(filler));
                     processedSources.push(audio);
                 }
                 return source;
@@ -35,19 +57,20 @@ function AudioGroup({sourcesInput}){
         setSources(processedSources);
     };
 
-    function stopMusic(){
-        const processedSources = sources.map((audio) => {
-            return audio.player.pause();
-        });
-        setSources(processedSources)
-        console.log("Sources ", sources)
-    }
+    useEffect(() => {
+        if(sourcesInput.length > 0 && !sourcesInput.includes("")){
+            checkSourceInList();
+            handleSources();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [sourcesInput]);
 
     useEffect(() => {
         sources.map((source, idx) =>{
             const filler = [...playing];
             filler[idx] = false
             source.player.addEventListener('ended', () => setPlaying(filler));
+            return null;
         })
         
         return () => {
@@ -56,9 +79,12 @@ function AudioGroup({sourcesInput}){
                 filler[idx] = false
                 source.player.removeEventListener('ended', () => setPlaying(filler));
                 source.player.pause()
+
+                return null;
             })
         };
-      }, [sources]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+      }, []);
 
     return(
         <div>
