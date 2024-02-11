3f43b430b93bb379754c169664529e9a720fe297
Nergan123 <moskalenkovladimir1998@gmail.com>
2024-02-11 01:25:21 +0100

    Image fullscreen mode implemented

--- frontend/src/gallery/imageSelected.js
+++ frontend/src/gallery/imageSelected.js
@@ -1,18 +1,35 @@
+import React, { useState } from 'react';
+
+
 function ImageSelected(props, idx) {
+
+    const [selected, setSelected] = useState(false);
 
     function handleClick() {
         console.log("Image clicked");
+        setSelected(!selected);
     }
 
-    const style = {
+    const styleNotSelected = {
         height: '15em',
+        zIndex: 1,
     }
 
+    const styleSelected = {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        height: `${window.innerHeight - 20}px`,
+        zIndex: 2,
+        backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    }
+
+    const style = selected ? styleSelected : styleNotSelected;
+    const imageSource = `data:image/jpeg;base64,${props.image.image}`;
+    
     return (
         <div>
-            <div className="ImageFrame" key={idx}>
-                <img src={`data:image/jpeg;base64,${props.image.image}`} onClick={handleClick} key={idx} style={style} />
-            </div>
+            <img className="ImageFrame" src={imageSource} onClick={handleClick} key={idx} style={style} />
             <p className="Title">{props.image.label}</p>
         </div>
     );
