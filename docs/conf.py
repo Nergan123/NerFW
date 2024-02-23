fad52e0a52f72e1d6f502065cfc35eb7fb27a1d2
Nergan123 <moskalenkovladimir1998@gmail.com>
2024-02-23 18:32:01 +0100

    Update doc requirements.txt

--- docs/conf.py
+++ docs/conf.py
@@ -5,8 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../nerfw'))
-sys.path.insert(0, os.path.abspath('../nerfw/game'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
