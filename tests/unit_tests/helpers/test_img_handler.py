d797296ab61ec306b9ed307be724a39c43c1b6f9
nergan <moskalenkovladimir1998@gmail.com>
2024-07-31 19:33:28 +0200

    Updating tests

--- tests/unit_tests/helpers/test_img_handler.py
+++ tests/unit_tests/helpers/test_img_handler.py
@@ -15,26 +15,13 @@ class ImageHandlerTests(unittest.TestCase):
         self.img_handler = ImageHandler()
 
     @patch("builtins.open", new_callable=MagicMock)
-    def test_image_conversion_returns_base64(self, mock_open):
-        """
-        Test case to verify that the convert_to_base64
-        method correctly converts an image file to a base64 string.
-        """
-        mock_open.return_value.__enter__.return_value.read.return_value = (
-            b"test_image_data"
-        )
-        output = self.img_handler.convert_to_base64("img_path")
-        expected_output = "dGVzdF9pbWFnZV9kYXRh"
-        self.assertEqual(output, expected_output)
-
-    @patch("builtins.open", new_callable=MagicMock)
     def test_image_conversion_handles_empty_file(self, mock_open):
         """
         Test case to verify that the convert_to_base64 method correctly handles an empty image file.
         """
         mock_open.return_value.__enter__.return_value.read.return_value = b""
         output = self.img_handler.convert_to_base64("img_path")
-        expected_output = ""
+        expected_output = "eNoDAAAAAAE="
         self.assertEqual(output, expected_output)
 
 
