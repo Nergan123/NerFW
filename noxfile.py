b7a244ad02ae8fa1e3c4904c589fdc5b88038272
nergan <moskalenkovladimir1998@gmail.com>
2024-09-05 22:03:14 +0200

    Base logic

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,8 @@ from nerfw import __version__
 
 @nox.session(python=None)
 def lint(session: nox.Session):
-    session.install("ruff")
+    session.install(".[dev]")
+    session.install(".")
     session.run("ruff", "check", ".")
 
 
