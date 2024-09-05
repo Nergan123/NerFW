1787c779891c945863e8912f0952b44114280d85
nergan <moskalenkovladimir1998@gmail.com>
2024-09-05 22:10:23 +0200

    Base logic

--- noxfile.py
+++ noxfile.py
@@ -1,22 +1,13 @@
-import sys
-from pathlib import Path
-
 import nox
-
-sys.path.insert(0, str(Path(__file__).parent))
-
-from nerfw import __version__
 
 
 @nox.session(python=None)
 def lint(session: nox.Session):
-    session.install(".[dev]")
-    session.install(".")
+    session.install("ruff")
     session.run("ruff", "check", ".")
 
 
 @nox.session(python=None)
 def towncrier(session: nox.Session):
     session.install("towncrier")
-    version = __version__
-    session.run("towncrier", "build", "--version", version, "--yes")
+    session.run("towncrier", "build", "--yes")
